560fcfd2f3f7244307db70c0ee6e2b964b212a95
Devesh Singh Chauhan <deveshsingh712005@gmail.com>
2026-04-24 10:31:08 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://authentication-sepia-one.vercel.app",
-    ],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
